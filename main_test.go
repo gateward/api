badaedfe571e8834360436f5b504638bc5f5ee4c
Alexandre Martin <alexandre@martin.pm>
2022-04-21 23:14:09 +0200

    test(main): add tests for main

--- main_test.go
+++ main_test.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World\n"))
+}
+
+func TestMain(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	w := httptest.NewRecorder()
+	handler(w, req)
+	res := w.Result()
+	defer res.Body.Close()
+	data, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("expected error to be nil got %v", err)
+	}
+	if string(data) != "Hello, World\n" {
+		t.Errorf("expected Hello, World got %v", string(data))
+	}
+}
